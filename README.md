e1ad48b8da681743767539b3c8ff268f21a7cf09
Balmore Ramirez <camaleoncode@gmail.com>
2022-07-26 21:34:18 -0600

    models

---
+++ README.md
@@ -0,0 +1,5 @@
+## librerias a utilizar 
+
+## Base de datos
+
+mongo atlas: https://cloud.mongodb.com/v2/62ae9a6cb652881974faa34e#clusters
---
+++ app.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require('express')
+const dbConnect = require("./config/mongo");
+const cors = require("cors");
+const app = express();
+
+
+app.use(cors());
+const port = process.env.PORT || 3000;
+
+app.listen(port, () =>
+    console.log(`http://localhost:${port}`)
+);
+
+dbConnect()
